43195ba0e1c85cf31fb515b66638a3201a52a741
ismoil27 <abduraimovismoil98@gmail.com>
2023-12-27 17:56:42 +0900

    feat: relationship between two models in django

--- store/models.py
+++ store/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+    
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
 
 
 
