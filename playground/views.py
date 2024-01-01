20e16c6d50dfc433e88481fe995edb052500e79b
ismoil27 <abduraimovismoil98@gmail.com>
2024-01-01 18:15:22 +0900

    feat: filtering data with F object

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    queryset = Product.objects.filter(inventory=F('unit_price'))
   
     return render(request, 'hello.html', {'products': list(queryset)})
