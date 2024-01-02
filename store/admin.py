1d942f245cec1644b5aebe8151fd26ef37f8ebd7
ismoil27 <abduraimovismoil98@gmail.com>
2024-01-02 14:54:09 +0900

    feat: developing search on the admin list page

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,8 @@
 from typing import Any
 from django.contrib import admin
 from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from . import models
 
 @admin.register(models.Product)
@@ -24,8 +26,9 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
    
 
@@ -41,7 +44,13 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist')
+            + '?'
+            + urlencode({
+                'collection_id':str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
     
 
     def get_queryset(self, request):
