4d6dc6d5a788ec70fc3ecdc03d31ba96e24c3848
ismoil27 <abduraimovismoil98@gmail.com>
2024-01-02 11:05:21 +0900

    feat: creating, updating & deleting item

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection, Cart,CartItem
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # queryset = Product.objects.order_by('-title')
-    TaggedItem.objects.get_tags_for(Product, 1)
+
+    # Creating a shopping cart with an item
+    cart = Cart()
+    cart.save()
+
+
+    item1 = CartItem()
+    item1.cart = cart
+    item1.product_id = 1
+    item1.quantity = 1
+    item1.save()
+
+# Updating the quantity of an item
+    item1 = CartItem.objects.get(pk=1)
+    item1.quantity = 2
+    item1.save()
+
+
+    # Removing a cart
+    cart = Cart(pk=1)
+    cart.delete()
     
+
     return render(request, 'hello.html', {'name': 'Jon'})
