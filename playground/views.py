e3206b5a1b5326a218848704c902854fd503f5ac
ismoil27 <abduraimovismoil98@gmail.com>
2024-01-02 10:17:53 +0900

    feat: using Manager model

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-
-from store.models import Product, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
     # queryset = Product.objects.order_by('-title')
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price=discounted_price
-    )
-
+    TaggedItem.objects.get_tags_for(Product, 1)
     
-    return render(request, 'hello.html', {'products': queryset})
+    return render(request, 'hello.html', {'name': 'Jon'})
